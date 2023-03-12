a9e420139a0a903ead93c466156c37a910cbe453
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-03-12 21:41:07 +0100

    Add files via upload

---
+++ clients/src/components/MyProfile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MyProfile = () => {
+    return (
+        <div>
+            <center>
+                Welcome to dashboard !!
+            </center>
+        </div>
+    )
+}
+
+export default MyProfile;
