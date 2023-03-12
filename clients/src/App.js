50c8cb6341cd9e27cb9b80077af22e8bb6311fe6
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-03-12 23:21:44 +0100

    Update App.js

--- clients/src/App.js
+++ clients/src/App.js
@@ -1,9 +1,25 @@
+import React, {useState, createContext} from 'react';
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import Nav from './Nav';
+import Login from './components/Login';
+import Register from './components/Register';
+import MyProfile from './components/MyProfile';
+
+export const store = createContext();
 
 function App() {
+  const [token, setToken] = useState(null);
   return (
-    <div className="App">
-      <h2>Hello World</h2>
+    <div>
+      <store.Provider value={[token, setToken]}>
+      <Nav/>
+      <Routes>
+        <Route exact path="/register" component={<Register/>}/>
+        <Route exact path="/login" component={<Login/>}/>
+        <Route exact path="/myprofile" component={<MyProfile/>}/>
+      </Routes>
+      </store.Provider>
     </div>
   );
 }
