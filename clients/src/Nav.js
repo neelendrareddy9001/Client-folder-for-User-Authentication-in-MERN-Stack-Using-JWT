e6980ab6ad56fcb8efd894a7816eb0ee1a11587b
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-03-12 20:41:14 +0100

    Add files via upload

---
+++ clients/src/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+        <ul>
+            <li>Register</li>
+            <li>Login</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Nav
---
+++ clients/src/components/Register.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+const Register = () => {
+    const [data, setData] = useState({
+        username : '',
+        email : '',
+        password : '',
+        confirmpassword : ''
+    })
+
+    const changeHandler = (e) => {
+        setData ({...data, [e.target.name]: e.target.value})
+    }
+  return (
+    <div>
+        <center>
+            <form>
+                <h3>Register</h3>
+                <input type="text" onChange={changeHandler} name="username" placeholder='User Name'/> <br/>
+                <input type="email" onChange={changeHandler} name="email" placeholder='Email'/> <br/>
+                <input type="password" onChange={changeHandler} name="password" placeholder='Password'/><br/>
+                <input type="password" onChange={changeHandler} name="confirmpassword" placeholder='Confirm Password'/><br/>
+                <input type='submit' value="Register"/> <br/>
+            </form>
+        </center>
+    </div>
+  )
+}
+
+export default Register
